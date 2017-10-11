21eb7d7e9074701d2f2708725022249d3dfed1f4
gijs bondue <gijsbondue@MacBook-Pro-van-gijs.local>
2017-10-11 14:45:10 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        $x= 10;
+        $y= 7;
+
+        echo $x + $y;
+        echo $x - $y;
+        echo $x * $y;
+        echo $x / $y;
+        echo $x % $y;
+    ?>
+
+</body>
+</html>
+
